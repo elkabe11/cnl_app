f080612f3df218543e3e7bc1a977ae699c0db71d
elkabe11 <38818024+elkabe11@users.noreply.github.com>
2018-04-28 17:24:21 -0300

    Update program.c

--- src/program.c
+++ src/program.c
@@ -1,12 +1,17 @@
 void setup(void)
 {
 	pinMode(21, OUTPUT);
+		pinMode(16, OUTPUT);
+
 }
 
 void loop(void)
 {
 	digitalWrite(21, HIGH);
+	digitalWrite(16, HIGH);
+
 	delay(200);
 	digitalWrite(21, LOW);
+	digitalWrite(16, LOW);
 	delay(200);
 }
